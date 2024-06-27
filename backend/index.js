4e5b603eb973e14556a0aba58cc6b3afb312af63
bud1gata <bud1gata@gmail.com>
2024-06-27 23:42:27 +0700

    init: update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,16 @@
 import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config/config.js" 
 import mongoose from "mongoose";
+import { Inventaris } from "./models/dbModels.js"
 
 const app = express();
 
 mongoose
     .connect(mongoDBURL)
     .then(() => {
-        console.log('App connected to database');
+        console.log('Backend terkoneksi ke database.');
         app.listen(PORT, () => {
-            console.log(`App running on port: ${PORT}`);
+            console.log(`Aplikasi berjalan pada port: ${PORT}`);
         });
     })
     .catch((error) => {
