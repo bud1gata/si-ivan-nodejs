1a8560dd0890fb72751a6ce0ca2505c978afc3dd
bud1gata <bud1gata@gmail.com>
2024-06-27 23:48:27 +0700

    init: add dependencies index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 import express, { request, response } from "express";
+import cors from "cors";
 import { PORT, mongoDBURL } from "./config/config.js" 
 import mongoose from "mongoose";
 import { Inventaris } from "./models/dbModels.js"
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 mongoose
     .connect(mongoDBURL)
     .then(() => {
-        console.log('Server Backend hidup dan terkoneksi ke Server database.');
+        console.log('Server Backend dapat berjalan dengan baik dan terkoneksi ke Server database.');
         app.listen(PORT, () => {
             console.log(`Aplikasi berjalan pada port: ${PORT}`);
         });
